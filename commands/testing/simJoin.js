2e2b404e00c8830a9c7db5c89ef655180dd325b8
Aaron Li-Mai <aaronmai@hotmail.com>
2020-11-04 10:18:21 -0700

    Fixed typo

--- commands/testing/simJoin.js
+++ commands/testing/simJoin.js
@@ -1,7 +1,7 @@
 module.exports = {
   commands: "simjoin",
   description: "Simulates a member joining the server.",
-  permissionError: "You must be an admininstrator to execute this command.",
+  permissionError: "You must be an administrator to execute this command.",
   requiredPermissions: ["ADMINISTRATOR"],
   callback: (msg, args, text, client) => {
     client.emit("guildMemberAdd", msg.member);
