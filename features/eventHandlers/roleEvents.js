e4bd0e8f0babbde3f13788c3b714c17ebcf4e56d
Aaron Li-Mai <aaronmai@hotmail.com>
2020-11-10 13:53:38 -0700

    Fixed a bug with the roleUpdate event

--- features/eventHandlers/roleEvents.js
+++ features/eventHandlers/roleEvents.js
@@ -23,17 +23,15 @@ module.exports = (client) => {
   });
 
   client.on("roleUpdate", async (oldRole, newRole) => {
-    if (oldRole.name !== newRole.name) {
-      msgEmbedEmbed
-        .setDescription(`**Role Name Changed**`)
-        .addFields(
-          { name: `**Before**`, value: `${oldRole.name}`, inline: true },
-          { name: `**After**`, value: `${newRole.name}`, inline: true }
-        )
-        .setAuthor(`${role.guild.name}`, role.guild.iconURL())
-        .setTimestamp()
-        .setFooter(`ID: ${role.id}`);
-      constants.sendMessageToBotThings(client, msgEmbed);
-    }
+    msgEmbed
+      .setDescription(`**Role Name Changed**`)
+      .addFields(
+        { name: `**Before**`, value: `${oldRole.name}`, inline: true },
+        { name: `**After**`, value: `${newRole.name}`, inline: true }
+      )
+      .setAuthor(`${newRole.guild.name}`, newRole.guild.iconURL())
+      .setTimestamp()
+      .setFooter(`ID: ${newRole.id}`);
+    constants.sendMessageToBotThings(client, msgEmbed);
   });
 };
