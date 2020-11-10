1e6d14d31fcf9e17ea402b3cfc9c3d7dc67ff9c2
Aaron Li-Mai <aaronmai@hotmail.com>
2020-11-10 15:30:01 -0700

    code refactoring

--- utils/firstMessage.js
+++ utils/firstMessage.js
@@ -1,15 +1,14 @@
-const { MessageEmbed } = require("discord.js");
-module.exports = async (client, id, msgEmbed, reactions = []) => {
+module.exports = async (client, id, msg, reactions = []) => {
   const channel = await client.channels.fetch(id);
 
   channel.messages.fetch().then((messages) => {
     if (messages.size === 0) {
-      channel.send(msgEmbed).then((message) => {
+      channel.send(msg).then((message) => {
         addReactions(message, reactions);
       });
     } else {
       for (const msg of messages) {
-        msg[1].edit(msgEmbed);
+        msg[1].edit(msg);
         addReactions(msg[1], reactions);
       }
     }
