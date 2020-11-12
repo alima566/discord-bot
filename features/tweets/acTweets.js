09bcfc54962587f27091f9333d8e447e635da112
Aaron Li-Mai <aaronmai@hotmail.com>
2020-11-12 15:20:12 -0700

    changed twitter accounts to display tweets from

---
+++ features/tweets/acTweets.js
@@ -0,0 +1,21 @@
+const T = require("@utils/twitterConfig");
+module.exports = (client) => {
+  var stream = T.stream("statuses/filter", {
+    follow: ["517358431", "1377451009"],
+  });
+
+  stream.on("tweet", (tweet) => {
+    const url = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`;
+    const channelID = "760894234973831188"; // #ac-news channel
+    try {
+      let channel = client.channels
+        .fetch(channelID)
+        .then((channel) => {
+          channel.send(url);
+        })
+        .catch((err) => console.log(err));
+    } catch (err) {
+      console.log(err);
+    }
+  });
+};
