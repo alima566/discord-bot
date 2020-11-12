f73e6105d52987988f5d9f32c96f13932785d8c7
Aaron Li-Mai <aaronmai@hotmail.com>
2020-11-12 15:46:13 -0700

    updated if statement

--- utils/twitterConfig.js
+++ utils/twitterConfig.js
@@ -8,15 +8,12 @@ module.exports = new Twit({
 });
 
 module.exports.isReply = (tweet) => {
-  if (
+  return (
     tweet.retweeted_status ||
     tweet.in_reply_to_status_id ||
     tweet.in_reply_to_status_id_str ||
     tweet.in_reply_to_user_id ||
     tweet.in_reply_to_user_id_str ||
     tweet.in_reply_to_screen_name
-  ) {
-    return true;
-  }
-  return false;
+  );
 };
