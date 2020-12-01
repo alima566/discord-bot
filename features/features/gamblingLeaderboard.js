03f00ee3590760b394f2306edad6113be40ee372
Aaron Li-Mai <aaronmai@hotmail.com>
2020-12-01 10:51:18 -0700

    added counter to show when leaderboard will update

--- features/features/gamblingLeaderboard.js
+++ features/features/gamblingLeaderboard.js
@@ -1,16 +1,21 @@
+const { MessageEmbed } = require("discord.js");
 const gamblingLeaderboardSchema = require("@schemas/gambling-leaderboard-schema");
 const gamblingSchema = require("@schemas/gambling-schema");
-const { MessageEmbed } = require("discord.js");
 const numeral = require("numeral");
 const moment = require("moment-timezone");
 
-const fetchTopMembers = async (guildID) => {
+const seconds = 5;
+const startingCounter = 60;
+let counter = startingCounter;
+let importantData = "";
+
+const fetchData = async (guildID) => {
   const timezone = moment.tz("America/New_York").format("z");
   const nextMonth = moment()
     .tz("America/New_York")
     .add(1, "months")
     .format("MMMM");
-  let text = `Person with the most points at the end of each month gets a free month of *Discord Nitro*. A winner is determined at 12AM ${timezone} on the first of every month.\n\n`;
+  importantData = `Person with the most points at the end of each month gets a free month of *Discord Nitro*. A winner is determined at 12AM ${timezone} on the first of every month.\n\n`;
   const results = await gamblingSchema
     .find({
       guildID,
@@ -21,51 +26,57 @@ const fetchTopMembers = async (guildID) => {
     .limit(10);
   for (let count = 0; count < results.length; count++) {
     const { userID, points = 0 } = results[count];
-    text += `${count + 1}. <@${userID}> has ${numeral(points).format("0,0")} ${
-      points !== 1 ? "points" : "point"
-    }.\n`;
+    importantData += `${count + 1}. <@${userID}> has ${numeral(points).format(
+      "0,0"
+    )} ${points !== 1 ? "points" : "point"}.\n`;
   }
-  text += `\nPoints will be reset back to 0 at 12AM ${timezone} on ${nextMonth} 1st.\n`;
-  return text;
+  importantData += `\nPoints will be reset back to 0 at 12AM ${timezone} on ${nextMonth} 1st.\n`;
 };
 
-const updateLeaderboard = async (client) => {
-  let msgEmbed = new MessageEmbed()
+const getText = () => {
+  const msgEmbed = new MessageEmbed()
     .setColor("#85bb65")
     .setTitle(`Gambling Leaderboard`)
     .setThumbnail(
       `https://cdn.glitch.com/2d031706-b85e-4c2b-8903-3af7e09dd1c4%2F310e3061-4bbb-400e-bb4e-59c6a4084a66_poker-chip.png?v=1604426732959`
-    );
+    )
+    .setDescription(importantData)
+    .setFooter(`Leaderboard will update in ${counter} seconds...`);
+  return msgEmbed;
+};
+
+const updateCounter = async (client) => {
+  let guildId = "";
   const results = await gamblingLeaderboardSchema.find({});
   for (const result of results) {
     const { channelID, _id: guildID } = result;
+    guildId = guildID;
     const guild = client.guilds.cache.get(guildID);
     if (guild) {
       const channel = client.channels.cache.get(channelID);
       if (channel) {
         const messages = await channel.messages.fetch();
         const firstMessage = messages.first();
-        const topMembers = await fetchTopMembers(guildID);
-        msgEmbed.setDescription(topMembers);
         if (firstMessage) {
-          const lastEditedAt = moment
-            .tz(firstMessage.editedAt, "America/New_York")
-            .format("YYYY/MM/DD HH:mm:ss z");
-          firstMessage.edit(
-            msgEmbed.setFooter(`Leaderboard last updated at: ${lastEditedAt}`)
-          );
+          firstMessage.edit(getText());
         } else {
-          channel.send(msgEmbed);
+          await channel.send(getText());
         }
       }
     }
   }
-
+  counter -= seconds;
+  if (counter <= 0) {
+    counter = startingCounter;
+    await fetchData(guildId);
+  }
   setTimeout(() => {
-    updateLeaderboard(client);
-  }, 1000 * 60);
+    updateCounter(client);
+  }, 1000 * seconds);
 };
 
 module.exports = async (client) => {
-  updateLeaderboard(client);
+  const guild = client.guilds.cache.get("707103910686621758");
+  await fetchData(guild.id);
+  updateCounter(client);
 };
