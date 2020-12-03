0f99bd40f3d9855c396b24cd988949f0c601dfe1
Aaron Li-Mai <aaronmai@hotmail.com>
2020-12-03 15:19:31 -0700

    code refactor

--- commands/moderation/say.js
+++ commands/moderation/say.js
@@ -1,20 +1,25 @@
 module.exports = {
   commands: ["say"],
   category: "Moderation",
-  expectedArgs: "<The leaderboard channel>",
+  expectedArgs: "<OPTIONAL: The tagged channel> <The message you want to say>",
   minArgs: 1,
   maxArgs: -1,
   description: "Makes the bot say whatever you type.",
   requiredPermissions: ["ADMINISTRATOR"],
   permissionError: "You must be an administrator to execute this command.",
   callback: async (msg, args, text) => {
-    const channel = msg.mentions.channels.first() || msg.channel;
-    let message = text;
-    if (args[0].includes("<#")) {
-      args.shift();
+    let message = "";
+    const channel = msg.mentions.channels.first();
+    if (channel) {
+      if (args.length === 1) {
+        msg.channel.send(`Please provide something to say.`);
+        return;
+      }
+      message = args.slice(1).join(" ");
+      channel.send(message);
+    } else {
       message = args.join(" ");
+      msg.channel.send(message);
     }
-    channel.send(message);
-    msg.delete();
   },
 };
