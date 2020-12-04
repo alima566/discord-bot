682a69083aba376d095f53f77d303db8e93d2e18
Aaron Li-Mai <aaronmai@hotmail.com>
2020-12-03 22:49:47 -0700

    code refactor

--- schemas/command-prefix-schema.js
+++ schemas/command-prefix-schema.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
+const reqString = {
+  type: String,
+  required: true,
+};
+
 const commandPrefixSchema = mongoose.Schema(
   {
     // Guild ID
-    _id: {
-      type: String,
-      required: true,
-    },
-    prefix: {
-      type: String,
-      required: true,
-    },
+    _id: reqString,
+    prefix: reqString,
   },
   {
     timestamps: true,
