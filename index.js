109ae64d0e254face9f3dc44f28fd2e41388b30c
Aaron Li-Mai <aaronmai@hotmail.com>
2020-12-14 08:31:55 -0700

    added christmas countdown to bot's status

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const Discord = require("discord.js");
 const WOKCommands = require("wokcommands");
 const { Player } = require("discord-player");
+const fetch = require("node-fetch");
 
 const client = new Discord.Client({
   partials: ["MESSAGE", "CHANNEL", "REACTION", "USER", "GUILD_MEMBER"],
@@ -13,11 +14,21 @@ const player = new Player(client);
 client.player = player;
 client.queue = new Map();
 
-client.on("ready", () => {
+client.on("ready", async () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity("https://www.twitch.tv/kelleebot", {
-    type: "WATCHING",
-  });
+  // client.user.setActivity("https://www.twitch.tv/kelleebot", {
+  //   type: "WATCHING",
+  // });
+  const daysToChristmas = await getDaysToChristmas();
+
+  client.user.setActivity(
+    `${daysToChristmas["Days to Christmas"]} ${
+      daysToChristmas !== 1 ? "days" : "day"
+    } until Christmas`,
+    {
+      type: "PLAYING",
+    }
+  );
 
   const wok = new WOKCommands(client, "commands", "features", "messages.json")
     .setMongoPath(process.env.MONGO_PATH)
@@ -38,5 +49,23 @@ client.on("ready", () => {
     console.log("Attempted to set language to", lang);
   });
 });
+
+const getDaysToChristmas = () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const body = await fetch(
+        "https://christmas-days.anvil.app/_/api/get_days"
+      );
+      const result = body.json();
+      if (result) {
+        resolve(result);
+      } else {
+        reject("An error occured.");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+};
 
 client.login(process.env.DISCORD_TOKEN);
