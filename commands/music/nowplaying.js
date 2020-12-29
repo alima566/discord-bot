43008e6eaba2a3d821eeb5c5542c6c041850283d
Aaron Li-Mai <aaronmai@hotmail.com>
2020-12-29 00:12:02 -0700

    actually show the currently playing song

--- commands/music/nowplaying.js
+++ commands/music/nowplaying.js
@@ -3,7 +3,9 @@ module.exports = {
   category: "Music",
   description: "Shows what's currently playing.",
   callback: async (msg) => {
-    msg.client.player.nowPlaying(msg);
-    return;
+    const nowPlaying = msg.client.player.nowPlaying(msg);
+    return msg.channel.send(
+      `Currently playing: **${nowPlaying.title}** - Requested by ${nowPlaying.requestedBy}`
+    );
   },
 };
