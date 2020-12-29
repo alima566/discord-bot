e847cc75abb69a9e371e2334a19409cf9155a8e4
Aaron Li-Mai <aaronmai@hotmail.com>
2020-12-28 23:54:30 -0700

    code refactor

--- commands/music/play.js
+++ commands/music/play.js
@@ -5,14 +5,13 @@ module.exports = {
   minArgs: 1,
   maxArgs: -1,
   description: "Plays the specified song.",
-  callback: async (msg, args) => {
+  callback: (msg, args) => {
     const voiceChannel = msg.member.voice.channel;
     if (!voiceChannel) {
       return msg.reply(
         `You need to be in a voice channel in order to play music!`
       );
     }
-    await msg.client.player.play(msg, args.join(" "), msg.member.user);
-    return;
+    return msg.client.player.play(msg, args.join(" "));
   },
 };
