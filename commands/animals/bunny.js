a6fb3a2f402674085b010cc38e217d14484cfab8
Aaron Li-Mai <aaronmai@hotmail.com>
2020-12-29 23:39:33 -0700

    don't use api

--- commands/animals/bunny.js
+++ commands/animals/bunny.js
@@ -1,29 +1,30 @@
-const animals = require("random-animals-api");
+//const animals = require("random-animals-api");
 const { MessageEmbed } = require("discord.js");
 const { log } = require("@utils/functions");
+const fetch = require("node-fetch");
 
 module.exports = {
   commands: ["bunny", "bunbun"],
   category: "Animals",
   cooldown: "15s",
-  description: "KelleeBot shows you a random picture of a bunny.",
+  description: "KelleeBot shows you a random picture/gif of a bunny.",
   callback: async (msg, args, text, client, prefix, instance) => {
     const { guild } = msg;
     let m = await msg.channel.send(
       instance.messageHandler.get(guild, "LOOKING_FOR_BUNNY")
     );
-    animals
-      .bunny()
-      .then((url) => {
-        let msgEmbed = new MessageEmbed()
+    fetch("https://api.bunnies.io/v2/loop/random/?media=gif,png")
+      .then((resp) => resp.json())
+      .then((data) => {
+        const msgEmbed = new MessageEmbed()
           .setColor("#006798")
           .setAuthor(
             `Bunny`,
             `https://cdn.glitch.com/310e3061-4bbb-400e-bb4e-59c6a4084a66%2Frabbit.png?v=1603768383901`,
-            `${url}`
+            `${data.media.gif}` //${data.media.poster}
           )
-          .setURL(url)
-          .setImage(url);
+          .setURL(`${data.media.gif}`)
+          .setImage(`${data.media.gif}`);
         m.edit(instance.messageHandler.get(guild, "FOUND_ANIMAL"));
         msg.channel.send(msgEmbed);
       })
@@ -34,5 +35,28 @@ module.exports = {
           `An error has occurred: ${e.message}`
         );
       });
+
+    // animals
+    //   .bunny()
+    //   .then((url) => {
+    //     let msgEmbed = new MessageEmbed()
+    //       .setColor("#006798")
+    //       .setAuthor(
+    //         `Bunny`,
+    //         `https://cdn.glitch.com/310e3061-4bbb-400e-bb4e-59c6a4084a66%2Frabbit.png?v=1603768383901`,
+    //         `${url}`
+    //       )
+    //       .setURL(url)
+    //       .setImage(url);
+    //     m.edit(instance.messageHandler.get(guild, "FOUND_ANIMAL"));
+    //     msg.channel.send(msgEmbed);
+    //   })
+    //   .catch((e) => {
+    //     log(
+    //       "ERROR",
+    //       "./commands/animals/bunny.js",
+    //       `An error has occurred: ${e.message}`
+    //     );
+    //   });
   },
 };
