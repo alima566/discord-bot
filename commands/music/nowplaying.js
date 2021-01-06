b2a5918b14928c59c31cb84025b1109db6c7dbd7
Aaron Li-Mai <aaronmai@hotmail.com>
2021-01-05 19:33:02 -0700

    Put msg in embed; added progress bar

--- commands/music/nowplaying.js
+++ commands/music/nowplaying.js
@@ -1,11 +1,24 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
-  commands: ["nowplaying", "np", "currentsong"],
+  commands: ["nowplaying", "np", "song"],
   category: "Music",
   description: "Shows what's currently playing.",
   callback: async ({ message }) => {
     const nowPlaying = message.client.player.nowPlaying(message);
-    return message.channel.send(
-      `Currently playing: **${nowPlaying.title}** - Requested by ${nowPlaying.requestedBy}`
-    );
+    if (!nowPlaying) return;
+
+    const progressBar = message.client.player.createProgressBar(message, {
+      timecodes: true,
+    });
+    const msgEmbed = new MessageEmbed()
+      .setColor("#1ED761")
+      .setAuthor(`Currently Playing`, `${message.guild.iconURL()}`)
+      .setDescription(
+        `[${nowPlaying.title}](${nowPlaying.url}) (${nowPlaying.duration})\n${progressBar}`
+      )
+      .setThumbnail(`${nowPlaying.thumbnail}`)
+      .setFooter(`Requested by ${nowPlaying.requestedBy.tag}`);
+    return message.channel.send(msgEmbed);
   },
 };
