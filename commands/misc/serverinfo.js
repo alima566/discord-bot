1b4005ad270a7b4dadd30f3cdcb6771606a2730c
Aaron Li-Mai <aaronmai@hotmail.com>
2021-01-12 20:52:30 -0700

    added serverinfo command

---
+++ commands/misc/serverinfo.js
@@ -0,0 +1,79 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  category: "Misc",
+  cooldown: "15s",
+  description: "Gives information about the server.",
+  callback: ({ message }) => {
+    const { guild, channel } = message;
+    const { name, region, owner, memberCount, createdAt } = guild;
+    const categoryChannels = guild.channels.cache.filter(
+      (channel) => channel.type === "category"
+    );
+    const totalTextChannels = guild.channels.cache.filter(
+      (c) => c.type === "text"
+    ).size;
+    const totalVoiceChannels = guild.channels.cache.filter(
+      (c) => c.type === "voice"
+    ).size;
+
+    const msgEmbed = new MessageEmbed()
+      .setColor("#DFBCF5")
+      .setAuthor(name, guild.iconURL())
+      .setThumbnail(guild.iconURL())
+      .addFields(
+        {
+          name: "**Owner**",
+          value: owner.user.tag,
+          inline: true,
+        },
+        {
+          name: "**Region**",
+          value: region,
+          inline: true,
+        },
+        {
+          name: "**Channel Categories**",
+          value: categoryChannels.size,
+          inline: true,
+        },
+        {
+          name: "**Text Channels**",
+          value: totalTextChannels,
+          inline: true,
+        },
+        {
+          name: "**Voice Channels**",
+          value: totalVoiceChannels,
+          inline: true,
+        },
+        {
+          name: "**Members**",
+          value: memberCount,
+          inline: true,
+        },
+        {
+          name: "**Roles**",
+          value: guild.roles.cache.size,
+          inline: false,
+        },
+        {
+          name: "**Role List**",
+          value: getRoleList(guild),
+          inline: true,
+        }
+      )
+      .setFooter(`ID: ${guild.id} | Server Created`)
+      .setTimestamp(createdAt);
+    channel.send(msgEmbed);
+  },
+};
+
+const getRoleList = (guild) => {
+  let roles = "";
+  guild.roles.cache.forEach((role) => {
+    roles += `${role.name}, `;
+  });
+  roles = roles.substr(0, roles.length - 2);
+  return roles;
+};
