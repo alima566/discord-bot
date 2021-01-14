4889e37c7cb79729224d29430951cfd5b90caadf
Aaron Li-Mai <aaronmai@hotmail.com>
2021-01-14 09:53:13 -0700

    show results in a menu if query returns multiple results

--- commands/misc/movie.js
+++ commands/misc/movie.js
@@ -17,64 +17,139 @@ module.exports = {
         include_adult: false,
       },
       (err, res) => {
-        try {
-          const movieID = res.results[0].id;
-          const movieName = res.results[0].original_title;
-          const movieLang = res.results[0].original_language;
-          const movieTitle = res.results[0].title;
-          const moviePoster = res.results[0].poster_path;
-          const movieReleaseDate = new Date(
-            res.results[0].release_date
-          ).toLocaleDateString("en-US", {
-            weekday: "long",
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-          });
-          const movieVoteAverage = res.results[0].vote_average;
-          const movieDescription = res.results[0].overview;
-
-          const msgEmbed = new MessageEmbed()
-            .setAuthor(
-              `${movieName} ${movieLang !== "en" ? `(${movieTitle})` : ""}`,
-              "https://pbs.twimg.com/profile_images/1243623122089041920/gVZIvphd_400x400.jpg",
-              `https://www.themoviedb.org/movie/${movieID}`
-            )
-            .setColor(0x337fd5)
-            .setThumbnail(
-              moviePoster
-                ? `https://image.tmdb.org/t/p/w500/${moviePoster}`
-                : null
-            )
-            .setDescription(
-              `${movieDescription}\n\nMore info: https://www.themoviedb.org/movie/${movieID}`
-            )
-            .addFields(
-              {
-                name: "**Release Date**",
-                value: movieReleaseDate,
-                inline: true,
-              },
-              {
-                name: "**Rating (out of 10)**",
-                value: movieVoteAverage,
-                inline: true,
-              }
-            )
-            .setFooter(
-              `Powered by TMDB`,
-              "https://pbs.twimg.com/profile_images/1243623122089041920/gVZIvphd_400x400.jpg"
-            );
-          channel.send(msgEmbed);
-        } catch (e) {
+        if (err) {
           log(
             "ERROR",
             "./commands/misc/movie.js",
-            `An error has occurred: ${e.message}`
+            `An error has occurred: ${err}`
           );
-          return channel.send(`I couldn't find the specified movie "${text}".`);
+          return channel.send(
+            `An error has occurred. Unable to fetch movie information.`
+          );
+        }
+
+        if (!res || res.length === 0) {
+          return channel.send(`No results were found for "${text}".`);
+        }
+
+        if (res.results[0] === undefined || !res.results[0]) {
+          return channel.send(`No results were found for "${text}".`);
+        }
+
+        if (res.results.length > 1) {
+          showAllMovies(text, res.results, message);
+        } else {
+          channel.send(showMovieInfo(res.results[0]));
         }
       }
     );
   },
+};
+
+const showAllMovies = (query, results, message) => {
+  let menuItems = "";
+  for (let i = 0; i < results.length; i++) {
+    let title = results[i].original_title;
+    let releaseYear =
+      results[i].release_date !== undefined
+        ? results[i].release_date.substr(0, 4)
+        : "";
+    menuItems += `${i + 1}. ${title} ${
+      releaseYear !== "" ? `(${releaseYear})` : ""
+    }\n`;
+  }
+
+  const embed = new MessageEmbed()
+    .setTitle(`Here are the search results for "${query}":`)
+    .setColor(0x337fd5)
+    .setDescription(menuItems)
+    .setFooter(
+      "Type the number of the movie you want to show information for."
+    );
+
+  message.channel.send(embed).then((msg) => {
+    const collector = msg.channel.createMessageCollector(
+      (m) => m.author.id === message.author.id,
+      {
+        time: 1000 * 10,
+        errors: ["time"],
+      }
+    );
+
+    collector.on("collect", (m) => {
+      if (
+        !isNaN(m.content) &&
+        parseInt(m.content) >= 1 &&
+        parseInt(m.content) <= results.length
+      ) {
+        m.delete();
+        const index = parseInt(m.content, 10);
+        const movie = results[index - 1];
+        collector.stop();
+        msg.edit(showMovieInfo(movie));
+      } else {
+        m.delete();
+        return message.channel
+          .send(
+            `Invalid selection. Please select a number from 1 to ${results.length}.`
+          )
+          .then((m) => m.delete({ timeout: 1000 * 2 }));
+      }
+    });
+
+    collector.on("end", (collected, reason) => {
+      if (reason === "time") {
+        return message.channel.send(`You did not choose a movie in time.`);
+      }
+    });
+  });
+};
+
+const showMovieInfo = (movie) => {
+  const movieID = movie.id;
+  const movieName = movie.original_title;
+  const movieLang = movie.original_language;
+  const movieTitle = movie.title;
+  const moviePoster = movie.poster_path;
+  const movieReleaseDate = new Date(movie.release_date).toLocaleDateString(
+    "en-US",
+    {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }
+  );
+  const movieVoteAverage = movie.vote_average;
+  const movieDescription = movie.overview;
+  const msgEmbed = new MessageEmbed()
+    .setAuthor(
+      `${movieName} ${movieLang !== "en" ? `(${movieTitle})` : ""}`,
+      "https://pbs.twimg.com/profile_images/1243623122089041920/gVZIvphd_400x400.jpg",
+      `https://www.themoviedb.org/movie/${movieID}`
+    )
+    .setColor(0x337fd5)
+    .setThumbnail(
+      moviePoster ? `https://image.tmdb.org/t/p/w500/${moviePoster}` : null
+    )
+    .setDescription(
+      `${movieDescription}\n\nMore info: https://www.themoviedb.org/movie/${movieID}`
+    )
+    .addFields(
+      {
+        name: "**Release Date**",
+        value: movieReleaseDate,
+        inline: true,
+      },
+      {
+        name: "**Rating (out of 10)**",
+        value: movieVoteAverage,
+        inline: true,
+      }
+    )
+    .setFooter(
+      `Powered by TMDB`,
+      "https://pbs.twimg.com/profile_images/1243623122089041920/gVZIvphd_400x400.jpg"
+    );
+  return msgEmbed;
 };
