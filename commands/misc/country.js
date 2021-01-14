621ce41da6f1f550a88db97f865cdecb4b63b392
Aaron Li-Mai <aaronmai@hotmail.com>
2021-01-14 09:17:13 -0700

    show search results if query returns multiple results

--- commands/misc/country.js
+++ commands/misc/country.js
@@ -10,76 +10,22 @@ module.exports = {
   description: "Gives you information about a country.",
   callback: async ({ message, text }) => {
     const { channel } = message;
-    fetch(`https://restcountries.eu/rest/v2/name/${text}?fullText=true`)
+    fetch(`https://restcountries.eu/rest/v2/name/${text}`)
       .then((resp) => resp.json())
       .then((data) => {
-        const countryName = data[0].name;
-        const countryPop = data[0].population;
-        const countryRegion = data[0].subregion;
-        const countryCapital = data[0].capital;
-        const countryDemonym = data[0].demonym;
-        const countryAreaKM = data[0].area;
-        const countryAreaM = parseInt(countryAreaKM * 0.62137).toLocaleString();
-        const countryNativeName = data[0].nativeName;
-        const countryCurrencyName = data[0].currencies[0].name;
-        const countryCurrencyCode = data[0].currencies[0].code;
-        const countryCurrencySymbol = data[0].currencies[0].symbol;
-        const countryCode = data[0].alpha3Code;
-        const countryFlag = `http://www.countryflags.io/${data[0].alpha2Code}/flat/64.png`;
-        const countryLanguages = data[0].languages
-          .map((l) => l.name)
-          .join(", ");
+        if (!data || data.length === 0) {
+          return channel.send(`No results were found for "${text}".`);
+        }
 
-        const msgEmbed = new MessageEmbed()
-          .setColor(0x337fd5)
-          .setAuthor(`Country Information - ${countryCode}`, countryFlag)
-          .setThumbnail(countryFlag)
-          .setTitle(countryName)
-          .addFields(
-            {
-              name: "**Population**",
-              value: countryPop.toLocaleString(),
-              inline: true,
-            },
-            {
-              name: "**Capital City**",
-              value: countryCapital === "" ? "-" : countryCapital,
-              inline: true,
-            },
-            {
-              name: "**Main Currency**",
-              value: `${countryCurrencyName} (${countryCurrencySymbol} ${countryCurrencyCode})`,
-              inline: true,
-            },
-            {
-              name: "**Located In**",
-              value: countryRegion,
-              inline: true,
-            },
-            {
-              name: "**Demonym**",
-              value: countryDemonym,
-              inline: true,
-            },
-            {
-              name: "**Native Name**",
-              value: countryNativeName,
-              inline: true,
-            },
-            {
-              name: "**Languages**",
-              value: countryLanguages,
-              inline: true,
-            },
-            {
-              name: "**Area**",
-              value: `${countryAreaKM.toLocaleString()}km (${countryAreaM}m)`,
-              inline: true,
-            }
-          )
-          .setFooter("Country data provided by restcountries.eu");
+        if (data[0] === undefined || !data[0]) {
+          return channel.send(`No results were found for "${text}".`);
+        }
 
-        channel.send(msgEmbed);
+        if (data.length > 1) {
+          showAllResults(text, data, message);
+        } else {
+          channel.send(showCountryInfo(data[0]));
+        }
       })
       .catch((e) => {
         log(
@@ -92,4 +38,129 @@ module.exports = {
         );
       });
   },
+};
+
+const showAllResults = (query, results, message) => {
+  let menuItems = "";
+  for (let i = 0; i < results.length; i++) {
+    menuItems += `${i + 1}. ${results[i].name}\n`;
+  }
+
+  const embed = new MessageEmbed()
+    .setTitle(`Here's what I found for "${query}":`)
+    .setColor(0x337fd5)
+    .setDescription(menuItems)
+    .setFooter(
+      "Type the number of the country you want to show information for."
+    );
+
+  message.channel.send(embed).then((msg) => {
+    const collector = msg.channel.createMessageCollector(
+      (m) => m.author.id === message.author.id,
+      {
+        time: 1000 * 10,
+        errors: ["time"],
+      }
+    );
+
+    collector.on("collect", (m) => {
+      if (
+        !isNaN(m.content) &&
+        parseInt(m.content) >= 1 &&
+        parseInt(m.content) <= results.length
+      ) {
+        m.delete();
+        const index = parseInt(m.content, 10);
+        const country = results[index - 1];
+        collector.stop();
+        msg.edit(showCountryInfo(country));
+      } else {
+        m.delete();
+        return message.channel
+          .send(
+            `Invalid selection. Please select a number from 1 to ${results.length}.`
+          )
+          .then((m) => m.delete({ timeout: 1000 * 2 }));
+      }
+    });
+
+    collector.on("end", (collected, reason) => {
+      if (reason === "time") {
+        return message.channel.send(`You did not choose a country in time.`);
+      }
+    });
+  });
+};
+
+const showCountryInfo = (data) => {
+  const countryName = data.name;
+  const countryPop = data.population;
+  const countryRegion = data.subregion;
+  const countryCapital = data.capital;
+  const countryDemonym = data.demonym;
+  const countryAreaKM = data.area || "-";
+  const countryAreaM =
+    countryAreaKM !== "-"
+      ? parseInt(countryAreaKM * 0.62137).toLocaleString()
+      : "-";
+  const countryNativeName = data.nativeName;
+  const countryCurrencyName = data.currencies[0].name;
+  const countryCurrencyCode = data.currencies[0].code;
+  const countryCurrencySymbol = data.currencies[0].symbol;
+  const countryCode = data.alpha3Code;
+  const countryFlag = `http://www.countryflags.io/${data.alpha2Code}/flat/64.png`;
+  const countryLanguages = data.languages.map((l) => l.name).join(", ");
+
+  const msgEmbed = new MessageEmbed()
+    .setColor(0x337fd5)
+    .setAuthor(`Country Information - ${countryCode}`, countryFlag)
+    .setThumbnail(countryFlag)
+    .setTitle(countryName)
+    .addFields(
+      {
+        name: "**Population**",
+        value: countryPop.toLocaleString(),
+        inline: true,
+      },
+      {
+        name: "**Capital City**",
+        value: countryCapital === "" ? "-" : countryCapital,
+        inline: true,
+      },
+      {
+        name: "**Main Currency**",
+        value: `${countryCurrencyName} (${countryCurrencySymbol} ${countryCurrencyCode})`,
+        inline: true,
+      },
+      {
+        name: "**Located In**",
+        value: countryRegion,
+        inline: true,
+      },
+      {
+        name: "**Demonym**",
+        value: countryDemonym,
+        inline: true,
+      },
+      {
+        name: "**Native Name**",
+        value: countryNativeName,
+        inline: true,
+      },
+      {
+        name: "**Languages**",
+        value: countryLanguages,
+        inline: true,
+      },
+      {
+        name: "**Area**",
+        value:
+          countryAreaKM === "-"
+            ? "-"
+            : `${countryAreaKM.toLocaleString()}km (${countryAreaM}m)`,
+        inline: true,
+      }
+    )
+    .setFooter("Country data provided by restcountries.eu");
+  return msgEmbed;
 };
