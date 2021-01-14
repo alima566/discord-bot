93debec4899877eb863f88a23c79595fd7e951ad
Aaron Li-Mai <aaronmai@hotmail.com>
2021-01-14 10:56:07 -0700

    moved to "fun" folder, changed category

---
+++ commands/fun/dadadvice.js
@@ -0,0 +1,23 @@
+const fetch = require("node-fetch");
+const { log } = require("@utils/functions");
+
+module.exports = {
+  commands: "dadadvice",
+  category: "Fun",
+  cooldown: "15s",
+  description: "KelleeBot gives you a random dad advice.",
+  callback: ({ message }) => {
+    fetch(`https://api.adviceslip.com/advice`) //`https://api.scorpstuff.com/advice.php`)
+      .then((response) => response.json())
+      .then((data) => {
+        message.channel.send(`${data["slip"]["advice"]}`);
+      })
+      .catch((e) => {
+        log(
+          "ERROR",
+          "./commands/misc/dadadvice.js",
+          `An error has occurred: ${e.message}`
+        );
+      });
+  },
+};
---
+++ commands/fun/dadjoke.js
@@ -0,0 +1,42 @@
+const fetch = require("node-fetch");
+const { log } = require("@utils/functions");
+
+module.exports = {
+  commands: "dadjoke",
+  category: "Fun",
+  cooldown: "15s",
+  description: "KelleeBot tells you a random dad joke.",
+  callback: async ({ message }) => {
+    let m = await message.channel.send(`Let me think of a dad joke...`);
+    fetch(`https://icanhazdadjoke.com/`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => m.edit(data.joke))
+      .catch((e) => {
+        log(
+          "ERROR",
+          "./commands/misc/dadjoke.js",
+          `An error has occurred: ${e.message}`
+        );
+      });
+  },
+  /*fetch(`https://dad-jokes.p.rapidapi.com/random/joke`, {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": process.env.X_RAPIDAPI_HOST,
+        "x-rapidapi-key": process.env.X_RAPIDAPI_KEY,
+        Accept: "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        m.edit(`${data.body[0].setup} ${data.body[0].punchline}`);
+      })
+      .catch(err => {
+        console.log(err);
+      });*/
+};
