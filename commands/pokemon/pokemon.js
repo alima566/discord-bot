c918125b0f9e25e41322853781a88a60abb043e8
Aaron Li-Mai <aaronmai@hotmail.com>
2021-01-13 17:49:29 -0700

    redid command

--- commands/pokemon/pokemon.js
+++ commands/pokemon/pokemon.js
@@ -1,101 +1,117 @@
-const fetch = require("node-fetch");
+const Pokedex = require("pokedex-promise-v2");
 const { MessageEmbed } = require("discord.js");
 const { log } = require("@utils/functions");
 
+const embedColor = {
+  normal: 0x9b9b6b,
+  fire: 0xe5711e,
+  water: 0x4c7bed,
+  electric: 0xf2c617,
+  grass: 0x69b741,
+  ice: 0x7fcece,
+  fighting: 0xaf2c25,
+  poison: 0x8e398e,
+  ground: 0xd9b34a,
+  flying: 0x9c88da,
+  psychic: 0xf7356f,
+  bug: 0x9ba91e,
+  rock: 0xa48f32,
+  ghost: 0x634e86,
+  dragon: 0x6124f5,
+  dark: 0x5e493c,
+  style: 0xa6a6c4,
+  fairy: 0xe484e4,
+};
+
 module.exports = {
-  commands: "pokemon",
   category: "Pokemon",
-  expectedArgs: "<pokemon_name> OR <pokedex_number>",
+  expectedArgs: "<pokemon_name>",
   minArgs: 1,
-  maxArgs: 1,
-  description:
-    "Retrieve information about a specific Pokémon. Command works with either the Pokémon name or its Pokédex number.",
+  description: "Retrieve information about a specific Pokémon.",
   cooldown: "15s",
-  callback: ({ message, args }) => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${args[0].toLowerCase()}/`)
-      .then((response) => response.json())
-      .then((data) => {
-        const pokeName = `${capFirstLetter(data.name)}`;
-        const image = `https://pokeres.bastionbot.org/images/pokemon/${data.id}.png`;
-        const pokedexLink = `https://www.pokemon.com/us/pokedex/${data.id}`;
-        const bulbapedia = `https://bulbapedia.bulbagarden.net/wiki/${pokeName}_(Pokémon)`;
-        const weight = data.weight / 10;
-        //console.log(data);
-        const msgEmbed = new MessageEmbed()
-          .setColor("DARK_AQUA")
-          .setURL(`${pokedexLink}`)
-          .setAuthor(`#${data.id} - ${pokeName}`, `${image}`, `${pokedexLink}`)
-          .setDescription(
-            `More info about ${pokeName} can be found here:
-               [Pokédex](${pokedexLink})
-               [Bulbapedia](${bulbapedia})`
-          )
-          .setThumbnail(`${image}`)
-          .addFields(
-            {
-              name: `**Type**`,
-              value:
-                getPokemonType(data.types).length > 0
-                  ? getPokemonType(data.types).join(", ")
-                  : "-",
-              inline: true,
-            },
-            {
-              name: `**Ability**`,
-              value:
-                getPokemonAbilites(data.abilities).length > 0
-                  ? getPokemonAbilites(data.abilities).join(", ")
-                  : "-",
-              inline: true,
-            },
-            {
-              name: `**Height**`,
-              value: `${data.height / 10} m`,
-              inline: true,
-            },
-            {
-              name: `**Weight**`,
-              value: `${weight.toFixed(1)} kg`,
-              inline: true,
-            }
-          );
-        message.channel.send(msgEmbed);
-      })
-      .catch((e) => {
-        message.channel.send(`I couldn't find that Pokémon :sob:`);
-        log(
-          "ERROR",
-          "./commands/pokemon/pokemon.js",
-          `An error has occurred: ${e.message}`
+  callback: async ({ message, text }) => {
+    const P = new Pokedex();
+    const pokeName = text.toLowerCase();
+
+    const { channel } = message;
+    try {
+      const result = await P.getPokemonByName(pokeName);
+      const height = result.height * 10;
+      const weight = result.weight / 10;
+
+      const msgEmbed = new MessageEmbed()
+        .setAuthor(
+          `#${result.id} - ${capFirstLetter(result.species.name)}`,
+          "http://pngimg.com/uploads/pokemon_logo/pokemon_logo_PNG12.png"
+        )
+        .setColor(embedColor[result.types[0].type.name])
+        .setThumbnail(result.sprites.front_default)
+        .addFields(
+          {
+            name: "**Height**",
+            value: `${toFeet(height)} (${height}cm)`,
+            inline: true,
+          },
+          {
+            name: "**Weight**",
+            value: `${kgToLbs(weight)} (${weight}kg)`,
+            inline: true,
+          },
+          {
+            name: result.types.length > 1 ? "**Types**" : "**Type**",
+            value: result.types
+              .map((t) => capFirstLetter(t.type.name))
+              .join(", "),
+            inline: true,
+          },
+          {
+            name: `**Abilities [${result.abilities.length}]**`,
+            value: result.abilities
+              .map((a) => capFirstLetter(a.ability.name))
+              .join(", "),
+            inline: true,
+          },
+          {
+            name: `**Stats**`,
+            value: result.stats
+              .map((s) => `${capFirstLetter(s.stat.name)} [${s.base_stat}]`)
+              .join(", "),
+            inline: true,
+          },
+          {
+            name: `**Moves [${result.moves.length}]**`,
+            value: result.moves
+              .map((m) => capFirstLetter(m.move.name))
+              .slice(0, 5)
+              .join(", "),
+            inline: true,
+          }
         );
-      });
+      channel.send(msgEmbed);
+    } catch (e) {
+      channel.send(`I couldn't find that Pokémon :sob:`);
+      log(
+        "ERROR",
+        "./commands/pokemon/pokemon.js",
+        `An error has occurred: ${e.message}`
+      );
+    }
   },
 };
 
 const capFirstLetter = (string) => {
-  var words = string.split(/(\s|-)+/),
-    output = [];
-
-  for (var i = 0, len = words.length; i < len; i += 1) {
-    output.push(words[i][0].toUpperCase() + words[i].toLowerCase().substr(1));
-  }
-
-  return output.join("");
-  //return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
-const getPokemonType = (pokeType) => {
-  let pokeTypeDesc = [];
-  pokeType.forEach((type) => {
-    pokeTypeDesc.push(capFirstLetter(type["type"]["name"]));
-  });
-  return pokeTypeDesc;
+const toFeet = (number) => {
+  const realFeet = (number * 0.3937) / 12;
+  const feet = Math.floor(realFeet);
+  const inches = Math.round((realFeet - feet) * 12);
+  return `${feet}ft ${inches}in`;
 };
 
-const getPokemonAbilites = (pokeAbilities) => {
-  let pokeAbilitiesDesc = [];
-  pokeAbilities.forEach((ability) => {
-    pokeAbilitiesDesc.push(capFirstLetter(ability["ability"]["name"]));
-  });
-  return pokeAbilitiesDesc;
+const kgToLbs = (pK) => {
+  const nearExact = pK / 0.45359237;
+  const lbs = Math.floor(nearExact);
+  return `${lbs}lbs`;
 };
