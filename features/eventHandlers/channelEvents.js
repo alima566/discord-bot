fee8fc912e34445e59780a14a09f7cf3448a4de2
Aaron Li-Mai <aaronmai@hotmail.com>
2021-02-07 19:27:11 -0700

    show who created/deleted/updated channels

--- features/eventHandlers/channelEvents.js
+++ features/eventHandlers/channelEvents.js
@@ -1,52 +1,77 @@
 const { MessageEmbed } = require("discord.js");
-const { sendMessageToBotThings } = require("@utils/functions");
+const { sendMessageToBotThings, fetchAuditLog } = require("@utils/functions");
 
 module.exports = (client) => {
   client.on("channelCreate", async (channel) => {
     let embed = null;
     if (channel.type.toLowerCase() === "dm") return;
+
+    const fetchedLogs = await fetchAuditLog(channel.guild, "CHANNEL_CREATE");
+    const channelCreationLog = fetchedLogs.entries.first();
+
     if (channel.type.toLowerCase() === "category") {
-      embed = createChannelEmbed(
-        channel,
-        null,
-        `**Category Created: ${channel.name}**`
-      );
+      if (channelCreationLog) {
+        const { executor } = channelCreationLog;
+        embed = await createChannelEmbed(
+          channel,
+          null,
+          `**Category Created:** ${channel.name}\n**Created By:** ${executor}`
+        );
+      }
     } else if (channel.type.toLowerCase() === "text") {
-      embed = createChannelEmbed(
-        channel,
-        null,
-        `**Channel Created: <#${channel.id}>**`
-      );
+      if (channelCreationLog) {
+        const { executor } = channelCreationLog;
+        embed = await createChannelEmbed(
+          channel,
+          null,
+          `**Text Channel Created:** ${channel}\n**Created By:** ${executor}`
+        );
+      }
     } else if (channel.type.toLowerCase() === "voice") {
-      embed = createChannelEmbed(
-        channel,
-        null,
-        `**Voice Channel Created: #${channel.name}**`
-      );
+      if (channelCreationLog) {
+        const { executor } = channelCreationLog;
+        embed = await createChannelEmbed(
+          channel,
+          null,
+          `**Voice Channel Created:** ${channel.name}\n**Created By:** ${executor}`
+        );
+      }
     }
     sendMessageToBotThings(client, channel.guild, embed);
   });
 
   client.on("channelDelete", async (channel) => {
     let embed = null;
+    const fetchedLogs = await fetchAuditLog(channel.guild, "CHANNEL_DELETE");
+    const channelDeletionLog = fetchedLogs.entries.first();
+
     if (channel.type.toLowerCase() === "category") {
-      embed = createChannelEmbed(
-        channel,
-        null,
-        `**Category Deleted: ${channel.name}**`
-      );
+      if (channelDeletionLog) {
+        const { executor } = channelDeletionLog;
+        embed = await createChannelEmbed(
+          channel,
+          null,
+          `**Category Deleted:** ${channel.name}\n**Deleted By:** ${executor}`
+        );
+      }
     } else if (channel.type.toLowerCase() === "text") {
-      embed = createChannelEmbed(
-        channel,
-        null,
-        `**Channel Deleted: #${channel.name}**`
-      );
+      if (channelDeletionLog) {
+        const { executor } = channelDeletionLog;
+        embed = await createChannelEmbed(
+          channel,
+          null,
+          `**Channel Deleted:** #${channel.name}\n**Deleted By:** ${executor}`
+        );
+      }
     } else if (channel.type.toLowerCase() === "voice") {
-      embed = createChannelEmbed(
-        channel,
-        null,
-        `**Voice Channel Deleted: ${channel.name}**`
-      );
+      if (channelDeletionLog) {
+        const { executor } = channelDeletionLog;
+        embed = await createChannelEmbed(
+          channel,
+          null,
+          `**Voice Channel Deleted:** ${channel.name}\n**Deleted By:** ${executor}`
+        );
+      }
     }
     sendMessageToBotThings(client, channel.guild, embed);
   });
@@ -55,19 +80,19 @@ module.exports = (client) => {
     let embed = null;
     if (oldChan.name !== newChan.name) {
       if (newChan.type.toLowerCase() === "category") {
-        embed = createChannelEmbed(
+        embed = await createChannelEmbed(
           oldChan,
           newChan,
           `**Category Name Changed**`
         );
       } else if (newChan.type.toLowerCase() === "text") {
-        embed = createChannelEmbed(
+        embed = await createChannelEmbed(
           oldChan,
           newChan,
           `**Channel Name Changed**`
         );
       } else if (newChan.type.toLowerCase() === "voice") {
-        embed = createChannelEmbed(
+        embed = await createChannelEmbed(
           oldChan,
           newChan,
           `**Voice Category Name Changed**`
@@ -78,21 +103,33 @@ module.exports = (client) => {
   });
 };
 
-const createChannelEmbed = (oldChan, newChan, description) => {
+const createChannelEmbed = async (oldChan, newChan, description) => {
   const embed = new MessageEmbed()
     .setColor("GREEN")
     .setDescription(description)
-    .setAuthor(`${oldChan.guild.name}`, oldChan.guild.iconURL())
-    .setTimestamp();
+    .setAuthor(
+      `${newChan === null ? oldChan.guild.name : newChan.guild.name}`,
+      `${newChan === null ? oldChan.guild.iconURL() : newChan.guild.iconURL()}`
+    )
+    .setTimestamp()
+    .setFooter(`ID: ${oldChan.id}`);
+
   if (newChan !== null) {
-    embed.addFields(
-      {
-        name: `**Before**`,
-        value: `${oldChan.name}`,
-        inline: true,
-      },
-      { name: `**After**`, value: `${newChan.name}`, inline: true }
-    );
+    const fetchedLogs = await fetchAuditLog(newChan.guild, "CHANNEL_UPDATE");
+    const channelUpdateLog = fetchedLogs.entries.first();
+
+    if (channelUpdateLog) {
+      const { executor } = channelUpdateLog;
+      embed.addFields(
+        {
+          name: `**Before**`,
+          value: oldChan.name,
+          inline: true,
+        },
+        { name: `**After**`, value: newChan.name, inline: true },
+        { name: `**Updated By**`, value: executor, inline: true }
+      );
+    }
   }
   return embed;
 };
