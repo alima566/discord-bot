7f4192dc6fea7fcc46a1777bd39cae7acff3d37a
Aaron Li-Mai <aaronmai@hotmail.com>
2021-02-07 18:24:48 -0700

    show who created/added/update roles

--- features/eventHandlers/roleEvents.js
+++ features/eventHandlers/roleEvents.js
@@ -1,24 +1,45 @@
 const { MessageEmbed } = require("discord.js");
-const { sendMessageToBotThings } = require("@utils/functions");
+const { sendMessageToBotThings, fetchAuditLog } = require("@utils/functions");
+const messageEvents = require("./messageEvents");
 
 module.exports = (client) => {
   client.on("roleCreate", async (role) => {
     const msgEmbed = new MessageEmbed()
       .setColor("YELLOW")
-      .setDescription(`**Role Created: ${role.name}**`)
       .setAuthor(`${role.guild.name}`, role.guild.iconURL())
       .setTimestamp()
       .setFooter(`ID: ${role.id}`);
+
+    const fetchedLogs = await fetchAuditLog(role.guild, "ROLE_CREATE");
+    const roleCreationLog = fetchedLogs.entries.first();
+    if (!roleCreationLog) {
+      msgEmbed.setDescription(`**Role Created:** \`${role.name}\``);
+    } else {
+      const { executor } = roleCreationLog;
+      msgEmbed.setDescription(
+        `**Role Created:** \`${role.name}\`\n**Role Created By:** ${executor}`
+      );
+    }
     sendMessageToBotThings(client, role.guild, msgEmbed);
   });
 
   client.on("roleDelete", async (role) => {
     const msgEmbed = new MessageEmbed()
       .setColor("YELLOW")
-      .setDescription(`**Role Deleted: ${role.name}**`)
       .setAuthor(`${role.guild.name}`, role.guild.iconURL())
       .setTimestamp()
       .setFooter(`ID: ${role.id}`);
+
+    const fetchedLogs = await fetchAuditLog(role.guild, "ROLE_DELETE");
+    const roleDeletionLog = fetchedLogs.entries.first();
+    if (!roleDeletionLog) {
+      msgEmbed.setDescription(`**Role Deleted:** \`${role.name}\``);
+    } else {
+      const { executor } = roleDeletionLog;
+      msgEmbed.setDescription(
+        `**Role Deleted:** \`${role.name}\`\n**Role Deleted By:** ${executor}`
+      );
+    }
     sendMessageToBotThings(client, role.guild, msgEmbed);
   });
 
@@ -29,14 +50,38 @@ module.exports = (client) => {
     ) {
       const msgEmbed = new MessageEmbed()
         .setColor("YELLOW")
-        .setDescription(`**Role Name Changed**`)
-        .addFields(
-          { name: `**Before**`, value: `${oldRole.name}`, inline: true },
-          { name: `**After**`, value: `${newRole.name}`, inline: true }
-        )
         .setAuthor(`${newRole.guild.name}`, newRole.guild.iconURL())
+        .setDescription(`**Role Name Changed**`)
         .setTimestamp()
         .setFooter(`ID: ${newRole.id}`);
+
+      const fetchedLogs = await fetchAuditLog(newRole.guild, "ROLE_CREATE");
+      const roleUpdateLog = fetchedLogs.entries.first();
+      if (!roleUpdateLog) {
+        msgEmbed.addFields(
+          { name: `**Before**`, value: `\`${oldRole.name}\``, inline: true },
+          { name: `**After**`, value: `\`${newRole.name}\``, inline: true }
+        );
+      } else {
+        const { executor } = roleUpdateLog;
+        msgEmbed.addFields(
+          {
+            name: `**Before**`,
+            value: `\`${oldRole.name}\``,
+            inline: true,
+          },
+          {
+            name: `**After**`,
+            value: `\`${newRole.name}\``,
+            inline: true,
+          },
+          {
+            name: `**Changed By**`,
+            value: executor,
+            inline: true,
+          }
+        );
+      }
       sendMessageToBotThings(client, newRole.guild, msgEmbed);
     }
   });
