9382fe438316d8e295d26fe5b75dc2e3ad2f01af
Aaron Li-Mai <aaronmai@hotmail.com>
2021-02-10 14:40:47 -0700

    added confirmation

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -21,6 +21,10 @@ module.exports = {
       return message.reply(`Nice try, but you can't kick yourself.`);
     }
 
+    if (!member.kickable) {
+      message.reply(`I do not have the permissions to kick that member.`);
+    }
+
     if (
       !guild.me.hasPermission("KICK_MEMBERS") ||
       !guild.me.hasPermission("BAN_MEMBERS")
@@ -30,29 +34,75 @@ module.exports = {
       );
     }
 
-    member
-      .kick(reason)
-      .then((mem) => {
-        channel.send(`Successfully kicked ${mem.user.tag} from the server.`);
+    channel
+      .send(
+        `Are you sure you want to kick **${member.user.tag}**${
+          reason ? ` for ${reason}` : ""
+        }? (Y/N)`
+      )
+      .then((msg) => {
+        const collector = msg.channel.createMessageCollector(
+          (m) => m.author.id === author.id,
+          {
+            time: 1000 * 10,
+            errors: ["time"],
+          }
+        );
 
-        const msgEmbed = new MessageEmbed()
-          .setColor("PURPLE")
-          .setAuthor(author.tag, author.displayAvatarURL())
-          .setThumbnail(mem.user.displayAvatarURL())
-          .setDescription(
-            `**Member:** ${mem.user.tag}\n**Action:** Kick${
-              reason !== "" ? `\n**Reason:** ${reason}` : ""
-            }`
-          )
-          .setTimestamp()
-          .setFooter(`ID: ${mem.id}`);
-        sendMessageToBotLog(client, guild, msgEmbed);
-      })
-      .catch((e) => {
-        if (!member.kickable) {
-          message.reply(`I do not have the permissions to kick that member.`);
-        }
-        return console.log(e.message);
+        collector.on("collect", (m) => {
+          switch (m.content.charAt(0).toUpperCase()) {
+            case "Y":
+              collector.stop();
+              kick(member, message, client, reason);
+              break;
+            case "N":
+              collector.stop();
+              channel.send(`**${member.user.tag}** was not kicked.`);
+              break;
+            default:
+              m.delete();
+              channel
+                .send(
+                  `Invalid selection. Please type either Y (Yes) or N (No).`
+                )
+                .then((m) => m.delete({ timeout: 1000 * 2 }));
+              break;
+          }
+        });
+
+        collector.on("end", (collected, reason) => {
+          if (reason === "time") {
+            return channel.send(
+              `You did not choose a response in time. **${member.user.tag}** was not kicked.`
+            );
+          }
+        });
       });
   },
+};
+
+const kick = (member, message, client, reason) => {
+  member
+    .kick(reason)
+    .then((mem) => {
+      message.channel.send(
+        `Successfully kicked **${mem.user.tag}** from the server.`
+      );
+
+      const msgEmbed = new MessageEmbed()
+        .setColor("PURPLE")
+        .setAuthor(message.author.tag, message.author.displayAvatarURL())
+        .setThumbnail(mem.user.displayAvatarURL())
+        .setDescription(
+          `**Member:** ${mem.user.tag}\n**Action:** Kick${
+            reason !== "" ? `\n**Reason:** ${reason}` : ""
+          }`
+        )
+        .setTimestamp()
+        .setFooter(`ID: ${mem.id}`);
+      sendMessageToBotLog(client, message.guild, msgEmbed);
+    })
+    .catch((e) => {
+      return console.log(e.message);
+    });
 };
