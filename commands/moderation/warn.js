c8fc7376cff9cc8dcb1682d7f946a22c4b867974
Aaron Li-Mai <aaronmai@hotmail.com>
2021-02-16 10:13:13 -0700

    added warn command

---
+++ commands/moderation/warn.js
@@ -0,0 +1,72 @@
+const { MessageEmbed } = require("discord.js");
+const { sendMessageToBotLog } = require("@utils/functions");
+const memberInfoSchema = require("@schemas/member-info-schema");
+const { default: messageHandler } = require("wokcommands/dist/message-handler");
+
+module.exports = {
+  category: "Moderation",
+  minArgs: 2,
+  description: "Warns a member from the server",
+  expectedArgs: "<The target's @ OR ID number> <Reason>",
+  requiredPermissions: ["MANAGE_SERVER"],
+  callback: async ({ message, args, client }) => {
+    const { guild, member } = message;
+
+    const target = message.mentions.users.first();
+    if (!target) {
+      return message.reply("Please specify someone to warn.");
+    }
+
+    args.shift();
+
+    const guildID = guild.id;
+    const userID = target.id;
+    const reason = args.join(" ");
+
+    if (!reason) {
+      return message.reply(
+        "Please provide a reason on why this member is being warned."
+      );
+    }
+
+    const warning = {
+      warnedBy: member.user.id,
+      timestamp: new Date().getTime(),
+      reason,
+      messageLink: message.url,
+    };
+
+    await memberInfoSchema.findOneAndUpdate(
+      {
+        guildID,
+        userID,
+      },
+      {
+        guildID,
+        userID,
+        $push: {
+          warnings: warning,
+        },
+      },
+      { upsert: true }
+    );
+
+    message.channel.send(`You have warned **${target.tag}** for ${reason}.`);
+    const msgEmbed = new MessageEmbed()
+      .setColor("#DFBE01")
+      .setAuthor(
+        message.author.tag,
+        message.author.displayAvatarURL({ dynamic: true })
+      )
+      .setThumbnail(target.displayAvatarURL({ dynamic: true }))
+      .setDescription(
+        `**Member:** ${target.tag}\n**Action:** Warn${
+          reason !== "" ? `\n**Reason:** ${reason}` : ""
+        }`
+      )
+      .setTimestamp()
+      .setFooter(`ID: ${target.id}`);
+
+    sendMessageToBotLog(client, message.guild, msgEmbed);
+  },
+};
