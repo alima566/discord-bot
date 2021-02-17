c690fabfbcb3c0f9589694b5846c3f0a23c07238
Aaron Li-Mai <aaronmai@hotmail.com>
2021-02-17 15:28:35 -0700

    code refactor

--- features/eventHandlers/guildEvents.js
+++ features/eventHandlers/guildEvents.js
@@ -3,25 +3,20 @@ const { MessageEmbed } = require("discord.js");
 
 module.exports = (client) => {
   client.on("guildBanAdd", async (guild, user) => {
-    const msgEmbed = new MessageEmbed().setColor("#CC0202");
     const fetchedLogs = await fetchAuditLog(guild, "MEMBER_BAN_ADD");
+    if (!fetchedLogs) {
+      const msgEmbed = createEmbed(
+        "#CC0202",
+        guild,
+        user,
+        `**${user.tag} has been banned from the server**`
+      );
+
+      return sendMessageToBotLog(client, guild, msgEmbed);
+    }
 
     const banLog = fetchedLogs.entries.first();
-    if (banLog) {
-      const { executor, target } = banLog;
-      msgEmbed
-        .setAuthor(guild.name, guild.iconURL())
-        .setDescription(
-          `${
-            target.id === user.id
-              ? `**${user.tag} has been banned from the server by ${executor}**`
-              : `**${user.tag} has been banned from the server**`
-          }`
-        )
-        .setTimestamp()
-        .setFooter(`ID: ${user.id}`);
-      return sendMessageToBotLog(client, guild, msgEmbed);
-    } else {
+    if (!banLog) {
       msgEmbed
         .setAuthor(guild.name, guild.iconURL())
         .setDescription(`**${user.tag} has been banned from the server**`)
@@ -29,36 +24,66 @@ module.exports = (client) => {
         .setFooter(`ID: ${user.id}`);
       return sendMessageToBotLog(client, guild, msgEmbed);
     }
+
+    const { executor, target } = banLog;
+    const msgEmbed = createEmbed(
+      "#CC0202",
+      guild,
+      user,
+      `${
+        target.id === user.id
+          ? `**${user.tag} has been banned from the server by ${executor}**`
+          : `**${user.tag} has been banned from the server**`
+      }`
+    );
+    sendMessageToBotLog(client, guild, msgEmbed);
   });
 
   client.on("guildBanRemove", async (guild, user) => {
-    const msgEmbed = new MessageEmbed().setColor("#33a532");
     const fetchedLogs = await fetchAuditLog(guild, "MEMBER_BAN_REMOVE");
-
-    const banLog = fetchedLogs.entries.first();
-    if (banLog) {
-      const { executor, target } = banLog;
-      msgEmbed
-        .setAuthor(guild.name, guild.iconURL())
-        .setDescription(
-          `${
-            target.id === user.id
-              ? `**${executor} removed ban from ${user.tag}**`
-              : `**Removed ban from ${user.tag}**`
-          }`
-        )
-        .setTimestamp()
-        .setFooter(`ID: ${user.id}`);
-      return sendMessageToBotLog(client, guild, msgEmbed);
-    } else {
-      msgEmbed
-        .setAuthor(guild.name, guild.iconURL())
-        .setDescription(`**Removed ban from ${user.tag}**`)
-        .setTimestamp()
-        .setFooter(`ID: ${user.id}`);
+    if (!fetchedLogs) {
+      const msgEmbed = createEmbed(
+        "#33a532",
+        guild,
+        user,
+        `**Removed ban from ${user.tag}**`
+      );
       return sendMessageToBotLog(client, guild, msgEmbed);
     }
+
+    const banLog = fetchedLogs.entries.first();
+    if (!banLog) {
+      const msgEmbed = createEmbed(
+        "#33a532",
+        guild,
+        user,
+        `**Removed ban from ${user.tag}**`
+      );
+      return sendMessageToBotLog(client, guild, msgEmbed);
+    }
+
+    const { executor, target } = banLog;
+    const msgEmbed = createEmbed(
+      "#33a532",
+      guild,
+      user,
+      `${
+        target.id === user.id
+          ? `**${executor} removed ban from ${user.tag}**`
+          : `**Removed ban from ${user.tag}**`
+      }`
+    );
+    sendMessageToBotLog(client, guild, msgEmbed);
   });
+};
+
+const createEmbed = (color, guild, user, description) => {
+  return new MessageEmbed()
+    .setColor(color)
+    .setAuthor(guild.name, guild.iconURL())
+    .setDescription(description)
+    .setTimestamp()
+    .setFooter(`ID: ${user.id}`);
 };
 
 module.exports.config = {
