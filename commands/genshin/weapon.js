87ab04ee8aaddeec5014b74935e9c42a28a262fd
Aaron Li-Mai <aaronmai@hotmail.com>
2021-03-03 15:07:40 -0700

    added genshin weapon command

---
+++ commands/genshin/weapon.js
@@ -0,0 +1,77 @@
+const genshin = require("genshin-db");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  category: "Genshin",
+  expectedArgs: "<weapon name>",
+  minArgs: 1,
+  description:
+    "Retrieve information about a specific weapon in Genshin Impact.",
+  cooldown: "15s",
+  callback: ({ message, text }) => {
+    const weapon = genshin.weapons(text);
+    if (!weapon) {
+      return message.reply(`I could not fidn a weapon by that name.`);
+    }
+
+    if (weapon.length) {
+      return message.reply(
+        `"${text}" returned more than one result. Please be more specific.`
+      );
+    }
+
+    const {
+      name,
+      weapontype,
+      rarity,
+      images,
+      baseatk,
+      substat,
+      subvalue,
+      description,
+      weaponmaterialtype,
+      url,
+    } = weapon;
+
+    const embed = new MessageEmbed()
+      .setColor("#355272")
+      .setAuthor(name, images.image, url)
+      .setThumbnail(images.image)
+      .setDescription(
+        `${description}\n\nMore info about ${name} can be found here:\n${url}`
+      )
+      .addFields(
+        {
+          name: "**Weapon Type**",
+          value: weapontype,
+          inline: true,
+        },
+        {
+          name: "**Rarity**",
+          value: rarity,
+          inline: true,
+        },
+        {
+          name: "**Base Attack (Lvl. 1)**",
+          value: baseatk,
+          inline: true,
+        },
+        {
+          name: "**Secondary Stat Type**",
+          value: substat,
+          inline: true,
+        },
+        {
+          name: "**Secondary Stat (Lvl. 1)**",
+          value: subvalue,
+          inline: true,
+        },
+        {
+          name: "**Weapon Material Type**",
+          value: weaponmaterialtype,
+          inline: true,
+        }
+      );
+    message.channel.send(embed);
+  },
+};
