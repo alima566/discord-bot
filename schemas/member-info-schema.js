1a888517506712a60c3633b65b499e2449d31aa1
Aaron Li-Mai <aaronmai@hotmail.com>
2021-03-26 10:32:55 -0600

    code refactor and added softban

--- schemas/member-info-schema.js
+++ schemas/member-info-schema.js
@@ -1,32 +1,24 @@
 const mongoose = require("mongoose");
+
 const reqString = {
   type: String,
+  required: true,
+};
+
+const reqObject = {
+  type: [Object],
   required: true,
 };
 
 const memberInfoSchema = mongoose.Schema({
   guildID: reqString,
   userID: reqString,
-  warnings: {
-    type: [Object],
-    required: true,
-  },
-  mutes: {
-    type: [Object],
-    required: true,
-  },
-  kicks: {
-    type: [Object],
-    required: true,
-  },
-  bans: {
-    type: [Object],
-    required: true,
-  },
-  unbans: {
-    type: [Object],
-    required: true,
-  },
+  warnings: reqObject,
+  mutes: reqObject,
+  kicks: reqObject,
+  softbans: reqObject,
+  bans: reqObject,
+  unbans: reqObject,
 });
 
 module.exports = mongoose.model("member-info", memberInfoSchema);
