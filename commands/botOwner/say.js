d0fee2da904cd735cbd5620cc11181fd85bb3512
Aaron Li-Mai <aaronmai@hotmail.com>
2021-04-01 09:48:42 -0600

    added the ability to add attachments

--- commands/botOwner/say.js
+++ commands/botOwner/say.js
@@ -1,7 +1,7 @@
 module.exports = {
   category: "Bot Owner",
-  expectedArgs: "<OPTIONAL: The tagged channel> <The message you want to say>",
-  minArgs: 1,
+  expectedArgs: "[The tagged channel] <The message you want to say>",
+  minArgs: 0,
   maxArgs: -1,
   description: "Makes the bot say whatever you type.",
   ownerOnly: true,
@@ -10,15 +10,32 @@ module.exports = {
     let msg = "";
     const channel = message.mentions.channels.first();
     if (channel) {
-      if (args.length === 1) {
+      if (args.length === 1 && message.attachments.size === 0) {
         return message.channel.send(`Please provide something to say.`);
       }
+
       msg = args.slice(1).join(" ");
-      channel.send(msg);
+      sendMessage(message, msg, { channel });
     } else {
       msg = args.join(" ");
-      message.channel.send(msg);
+      sendMessage(message, msg);
     }
     message.delete();
   },
+};
+
+const sendMessage = (message, content, options) => {
+  let channel = message.channel;
+  if (options) {
+    if (options.channel) {
+      channel = options.channel;
+    }
+  }
+
+  if (message.attachments.size > 0) {
+    const attachment = message.attachments.first();
+    channel.send(content, attachment); //Only send the attachment and not any text
+  } else {
+    channel.send(content);
+  }
 };
