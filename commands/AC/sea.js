c1cee8f1ca10e5de02cec71b93aa1c8d348352df
Aaron Li-Mai <aaronmai@hotmail.com>
2021-05-10 15:34:23 -0600

    code refactor and converted to slash command

--- commands/AC/sea.js
+++ commands/AC/sea.js
@@ -8,7 +8,7 @@ const {
 } = require("@utils/functions");
 
 module.exports = {
-  commands: "sea",
+  slash: "both",
   category: "🍀 AC",
   expectedArgs: "<seacreature_name>",
   minArgs: 1,
@@ -16,77 +16,79 @@ module.exports = {
   description:
     "Retrieve information about a specific sea creature in *Animal Crossing: New Horizons*.",
   cooldown: "15s",
-  callback: ({ message, text }) => {
+  callback: async ({ message, text }) => {
     text = text.trim();
     if (text.includes(" ")) {
       text = text.replace(/ +/g, "_");
     }
-    fetch(`https://api.nookipedia.com/nh/sea/${text.toLowerCase()}`, {
-      method: "GET",
-      headers: {
-        "X-API-KEY": process.env.NOOK_API_KEY,
-        "Accept-Version": "2.0.0",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        const msgEmbed = new MessageEmbed()
-          .setColor("AQUA")
-          .setURL(`${data.url}`)
-          .setAuthor(`${data.name}`, `${data.image_url}`, `${data.url}`)
-          .setDescription(
-            `More info about the ${data.name} can be found here:\n${data.url}`
-          )
-          .setThumbnail(`${data.image_url}`)
-          .addFields(
-            {
-              name: `**Price**`,
-              value: `${numeral(data.sell_nook).format("0,0")}`,
-              inline: true,
-            },
-            {
-              name: `**Shadow**`,
-              value: `${data.shadow_size}`,
-              inline: true,
-            },
-            /*{
-              name: `**Rarity**`,
-              value: `${data.rarity}`,
-              inline: true
-            },*/
-            {
-              name: `**Shadow Movement**`,
-              value: `${data.shadow_movement}`,
-              inline: true,
-            },
-            {
-              name: `**Months Available**`,
-              value: `North:\n${getMonthsAvailable(
-                data.north
-              )}\nSouth:\n${getMonthsAvailable(data.south)}`,
-              inline: true,
-            },
-            {
-              name: `**Time Available**`,
-              value: `North:\n${getTimesAvailable(
-                data.north
-              )}\nSouth:\n${getTimesAvailable(data.south)}`,
-              inline: true,
-            }
-          )
-          .setFooter(
-            `Powered by Nookipedia`,
-            `https://nookipedia.com/wikilogo.png`
-          );
-        message.channel.send(msgEmbed);
-      })
-      .catch((e) => {
-        message.channel.send(`I couldn't find that sea creature :sob:`);
-        log(
-          "ERROR",
-          "./commands/AC/sea.js",
-          `An error has occurred: ${e.message}`
+
+    try {
+      const resp = await fetch(
+        `https://api.nookipedia.com/nh/sea/${text.toLowerCase()}`,
+        {
+          method: "GET",
+          headers: {
+            "X-API-KEY": process.env.NOOK_API_KEY,
+            "Accept-Version": "2.0.0",
+          },
+        }
+      );
+      const data = await resp.json();
+      const {
+        url,
+        name,
+        image_url,
+        sell_nook,
+        shadow_size,
+        shadow_movement,
+        north,
+        south,
+      } = data;
+
+      const msgEmbed = new MessageEmbed()
+        .setColor("AQUA")
+        .setURL(url)
+        .setAuthor(name, image_url, url)
+        .setDescription(
+          `More info about the ${name} can be found here:\n${url}`
+        )
+        .setThumbnail(image_url)
+        .addFields(
+          {
+            name: "**Price**",
+            value: numeral(sell_nook).format("0,0"),
+            inline: true,
+          },
+          { name: "**Shadow Size**", value: shadow_size, inline: true },
+          { name: "**Shadow Movement**", value: shadow_movement, inline: true },
+          {
+            name: `**Months Available**`,
+            value: `North:\n${getMonthsAvailable(
+              north
+            )}\nSouth:\n${getMonthsAvailable(south)}`,
+            inline: true,
+          },
+          {
+            name: `**Time Available**`,
+            value: `North:\n${getTimesAvailable(
+              north
+            )}\nSouth:\n${getTimesAvailable(south)}`,
+            inline: true,
+          }
+        )
+        .setFooter(
+          `Powered by Nookipedia`,
+          `https://nookipedia.com/wikilogo.png`
         );
-      });
+      return message ? message.channel.send(msgEmbed) : msgEmbed;
+    } catch (e) {
+      const errorMsg = "I couldn't find that sea creature :sob:";
+      log(
+        "ERROR",
+        "./commands/AC/sea.js",
+        `An error has occurred: ${e.message}`
+      );
+      return message ? message.channel.send(errorMsg) : errorMsg;
+    }
   },
 };
