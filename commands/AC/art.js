37caf2419940bf9743a2cd29f1eddafd17ed94be
Aaron Li-Mai <aaronmai@hotmail.com>
2021-05-10 14:42:57 -0600

    code refactor and can be used as slash command now

--- commands/AC/art.js
+++ commands/AC/art.js
@@ -3,78 +3,93 @@ const { MessageEmbed } = require("discord.js");
 const numeral = require("numeral");
 const { log } = require("@utils/functions");
 
+const errorMsg = "I couldn't find that artwork :sob:";
+
 module.exports = {
-  commands: "art",
+  slash: "both",
   category: "🍀 AC",
-  expectedArgs: "<artwork_name>",
+  expectedArgs: "<Artwork Name>",
   minArgs: 1,
-  //maxArgs: 1,
   description:
     "Retrieve information about a specific artwork in *Animal Crossing: New Horizons*.",
   cooldown: "15s",
-  callback: ({ message, text }) => {
+  callback: async ({ message, text }) => {
     text = text.trim();
     if (text.includes(" ")) {
       text = text.replace(/ +/g, "_");
     }
-    fetch(`https://api.nookipedia.com/nh/art/${text.toLowerCase()}`, {
-      method: "GET",
-      headers: {
-        "X-API-KEY": process.env.NOOK_API_KEY,
-        "Accept-Version": "2.0.0",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.title === "No data was found for the given query.") {
-          return message.reply(`I couldn't find that artwork :sob:`);
-        }
 
-        const msgEmbed = new MessageEmbed()
-          .setColor("DARK_RED")
-          .setURL(`${data.url}`)
-          .setAuthor(`${data.name}`, `${data.image_url}`, `${data.url}`)
-          .setDescription(
-            `More info about the ${data.name} can be found here:\n${data.url}`
-          )
-          .setThumbnail(`${data.image_url}`)
-          .addFields(
-            {
-              name: `**Buy Price**`,
-              value: `${numeral(data.buy).format("0,0")}`,
-              inline: true,
-            },
-            {
-              name: `**Sell Price**`,
-              value: `${numeral(data.sell).format("0,0")}`,
-              inline: true,
-            },
-            {
-              name: `**Has Fake**`,
-              value: data.has_fake ? "Yes" : "No",
-              inline: true,
-            },
-            {
-              name: `**Authenticity**`,
-              value: !data.authenticity
-                ? `This artwork is always genuine.`
-                : data.authenticity,
-              inline: true,
-            }
-          )
-          .setFooter(
-            `Powered by Nookipedia`,
-            `https://nookipedia.com/wikilogo.png`
-          );
-        message.channel.send(msgEmbed);
-      })
-      .catch((e) => {
-        message.channel.send(`I couldn't find that artwork :sob:`);
-        log(
-          "ERROR",
-          "./commands/AC/art.js",
-          `An error has occurred: ${e.message}`
+    try {
+      const resp = await fetch(
+        `https://api.nookipedia.com/nh/art/${text.toLowerCase()}`,
+        {
+          method: "GET",
+          headers: {
+            "X-API-KEY": process.env.NOOK_API_KEY,
+            "Accept-Version": "2.0.0",
+          },
+        }
+      );
+      const data = await resp.json();
+      const {
+        title,
+        url,
+        name,
+        image_url,
+        buy,
+        sell,
+        has_fake,
+        authenticity,
+      } = data;
+
+      if (title === "No data was found for the given query.") {
+        return message ? message.channel.send(errorMsg) : errorMsg;
+      }
+
+      const msgEmbed = new MessageEmbed()
+        .setColor("RED")
+        .setURL(url)
+        .setAuthor(name, image_url, url)
+        .setDescription(
+          `More info about the ${name} can be found here:\n${url}`
+        )
+        .setThumbnail(image_url)
+        .addFields(
+          {
+            name: "**Buy Price**",
+            value: numeral(buy).format("0,0"),
+            inline: true,
+          },
+          {
+            name: "**Sell Price**",
+            value: numeral(sell).format("0,0"),
+            inline: true,
+          },
+          {
+            name: "**Has Fake**",
+            value: has_fake ? "Yes" : "No",
+            inline: true,
+          },
+          {
+            name: "**Authenticity**",
+            value: authenticity
+              ? authenticity
+              : "This artwork is always genuine.",
+            inline: true,
+          }
+        )
+        .setFooter(
+          `Powered by Nookipedia`,
+          `https://nookipedia.com/wikilogo.png`
         );
-      });
+      return message ? message.channel.send(msgEmbed) : msgEmbed;
+    } catch (e) {
+      log(
+        "ERROR",
+        "./commands/AC/art.js",
+        `An error has occurred: ${e.message}`
+      );
+      return message ? message.channel.send(errorMsg) : errorMsg;
+    }
   },
 };
