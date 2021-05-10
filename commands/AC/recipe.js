f14c5c54313db98ed5c9b8c9876e496c094913b8
Aaron Li-Mai <aaronmai@hotmail.com>
2021-05-10 15:20:43 -0600

    code refactor and converted to slash command

--- commands/AC/recipe.js
+++ commands/AC/recipe.js
@@ -4,71 +4,75 @@ const { log } = require("@utils/functions");
 
 module.exports = {
   commands: ["recipe", "diy"],
+  slash: "both",
   category: "🍀 AC",
-  expectedArgs: "<diy_name>",
+  expectedArgs: "<recipe>",
   minArgs: 1,
-  //maxArgs: 1,
   description:
     "Retrieve information about a specific recipe in *Animal Crossing: New Horizons*.",
   cooldown: "15s",
-  callback: ({ message, text }) => {
+  callback: async ({ message, text }) => {
     text = text.trim();
     if (text.includes(" ")) {
       text = text.replace(/ +/g, "_");
     }
-    fetch(`https://api.nookipedia.com/nh/recipes/${text.toLowerCase()}`, {
-      method: "GET",
-      headers: {
-        "X-API-KEY": process.env.NOOK_API_KEY,
-        "Accept-Version": "2.0.0",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        const msgEmbed = new MessageEmbed()
-          .setColor("#8F5707")
-          .setURL(`${data.url}`)
-          .setAuthor(`${data.name}`, `${data.image_url}`, `${data.url}`)
-          .setDescription(
-            `More info about the ${data.name} can be found here:\n${data.url}`
-          )
-          .setThumbnail(`${data.image_url}`)
-          .addFields(
-            {
-              name: `**Materials**`,
-              value: `${getAllMaterials(data)}`,
-              inline: true,
-            },
-            {
-              name: `**Obtained From**`,
-              value: `${getAvailabilty(data)}`,
-              inline: true,
-            },
-            {
-              name: `**Note**`,
-              value: `${
-                getAvailabiltyNote(data).length !== 1
-                  ? getAvailabiltyNote(data)
-                  : "-"
-              }`,
-              inline: true,
-            }
-          )
-          .setFooter(
-            `Powered by Nookipedia`,
-            `https://nookipedia.com/wikilogo.png`
-          );
-        message.channel.send(msgEmbed);
-      })
-      .catch((e) => {
-        message.channel.send(`I couldn't find that DIY :sob:`);
-        log(
-          "ERROR",
-          "./commands/AC/recipe.js",
-          `An error has occurred: ${e.message}`
+
+    try {
+      const resp = await fetch(
+        `https://api.nookipedia.com/nh/recipes/${text.toLowerCase()}`,
+        {
+          method: "GET",
+          headers: {
+            "X-API-KEY": process.env.NOOK_API_KEY,
+            "Accept-Version": "2.0.0",
+          },
+        }
+      );
+      const data = await resp.json();
+      const { url, name, image_url } = data;
+
+      const msgEmbed = new MessageEmbed()
+        .setColor("#8F5707")
+        .setURL(url)
+        .setAuthor(name, image_url, url)
+        .setDescription(
+          `More info about the ${name} can be found here:\n${url}`
+        )
+        .setThumbnail(image_url)
+        .addFields(
+          {
+            name: "**Materials**",
+            value: getAllMaterials(data),
+            inline: true,
+          },
+          {
+            name: "**Obtained From**",
+            value: getAvailabilty(data),
+            inline: true,
+          },
+          {
+            name: "**Note**",
+            value:
+              getAvailabiltyNote(data).length !== 1
+                ? getAvailabiltyNote(data)
+                : "-",
+            inline: true,
+          }
+        )
+        .setFooter(
+          `Powered by Nookipedia`,
+          `https://nookipedia.com/wikilogo.png`
         );
-      });
+      return message ? message.channel.send(msgEmbed) : msgEmbed;
+    } catch (e) {
+      const errorMsg = "I couldn't find that DIY :sob:";
+      log(
+        "ERROR",
+        "./commands/AC/recipe.js",
+        `An error has occurred: ${e.message}`
+      );
+      return message ? message.channel.send(errorMsg) : errorMsg;
+    }
   },
 };
 
